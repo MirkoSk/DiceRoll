72429cc3c163cbdc6b9afbb1e3146148c3a4cc13
Mirko <mirkoskroch@gmail.com>
2020-12-16 00:43:29 +0100

    Initial commit

---
+++ Assets/Scripts/DiceRoller.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoller : MonoBehaviour
+{
+    [SerializeField] GameObject dicePrefab = null;
+    [SerializeField] Transform spawnLocation = null;
+    [SerializeField] Transform throwTarget = null;
+
+    [Header("Throw Dynamics")]
+    [SerializeField] float throwSpeed = 10f;
+    [SerializeField] float throwSpeedVariance = 2f;
+    [SerializeField] float angularSpeedInThrowDirection = 10f;
+    [Tooltip("Angular speed perpendicular to the throw direction.")]
+    [SerializeField] float angularSpeedVariance = 7f;
+    [SerializeField] float spaceBetweenDice = 0.7f;
+
+
+
+    int numberOfDiceToRoll = 1;
+    List<GameObject> currentDice = new List<GameObject>();
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+
+
+
+    public void RollDice()
+    {
+        foreach (GameObject die in currentDice)
+        {
+            Destroy(die);
+        }
+        currentDice.Clear();
+
+        for (int i = 0; i < numberOfDiceToRoll; i++)
+        {
+            Vector3 spawnPosition = spawnLocation.position 
+                + Vector3.forward * (i / 2) * spaceBetweenDice
+                + Vector3.right * (i % 2) * spaceBetweenDice;
+
+            GameObject die = Instantiate(dicePrefab, spawnPosition, Random.rotation);
+            Rigidbody dieRB = die.GetComponent<Rigidbody>();
+
+            Vector3 throwDirection = (throwTarget.position - spawnPosition).normalized;
+            dieRB.velocity = throwDirection * (throwSpeed + Random.Range(-throwSpeedVariance, throwSpeedVariance));
+
+            Vector3 angularVelocity = Vector3.Cross(throwDirection, Vector3.down) * angularSpeedInThrowDirection;
+            angularVelocity += throwDirection * Random.Range(-angularSpeedVariance, angularSpeedVariance);
+            if (dieRB.maxAngularVelocity < angularSpeedInThrowDirection) dieRB.maxAngularVelocity = angularSpeedInThrowDirection;
+            dieRB.angularVelocity = angularVelocity;
+
+            currentDice.Add(die);
+        }
+    }
+
+    public void UpdateNumberOfDiceToRoll(string number)
+    {
+        numberOfDiceToRoll = int.Parse(number);
+    }
+}
---
+++ Assets/Scripts/DieController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieController : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource = null;
+    [SerializeField] AudioClip dieRollSound = null;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (audioSource && dieRollSound && collision.transform.tag == "RollBox") audioSource.PlayOneShot(dieRollSound);
+    }
+}
