7562a2e83365a46bfa3865f1c6559119571f3478
Mirko <mirkoskroch@gmail.com>
2020-12-17 21:42:43 +0100

    Different versions of SpinningWheels

---
+++ Assets/Scripts/SpinningBarrel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpinningBarrel : MonoBehaviour
+{
+    [SerializeField] float torque = 1000f;
+
+    new Rigidbody rigidbody;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        SpinTheWheel();
+    }
+
+
+
+    public void SpinTheWheel()
+    {
+        rigidbody.AddRelativeTorque(new Vector3(0f, torque, 0f), ForceMode.VelocityChange);
+    }
+}
